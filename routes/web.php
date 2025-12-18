b281ad25646c51b1bb9a78260f7ab1834c76cdf2
Felipe Genuino <design@felipegenuino.com>
2025-12-18 11:20:49 -0300

    update

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,51 @@ Route::get('/', function () {
 });
 
 Route::get('/feed', function () {
-    return view('feed');
+    $feedItems = json_decode(json_encode([
+        [
+            'postedDateTime' => '3h',
+            'content' =>  <<<'str'
+                <p>
+                  I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                </p>
+                <img src="/images/simon-chilling.png" alt="" />
+            str,
+            'likesCount' => 24,
+            'replyCount' => 45,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/images/adrian.png',
+                'displayName' => 'adrian',
+                'handle' => '@adrian_dev',
+                'content' => 'Just finished a new project! Check it out on my portfolio.',
+            ],
+
+        ]
+    ]));
+    return view('feed', compact('feedItems'));
 });
 
 Route::get('/profile', function () {
-    return view('profile');
+    $feedItems = json_decode(json_encode([
+        [
+            'postedDateTime' => '3h',
+            'content' =>  <<<'str'
+                <p>
+                  I made this! <a href="#">#myartwork</a> <a href="#">#pixl</a>
+                </p>
+                <img src="/images/simon-chilling.png" alt="" />
+            str,
+            'likesCount' => 24,
+            'replyCount' => 45,
+            'repostCount' => 151,
+            'profile' => [
+                'avatar' => '/images/adrian.png',
+                'displayName' => 'adrian',
+                'handle' => '@adrian_dev',
+                'content' => 'Just finished a new project! Check it out on my portfolio.',
+            ],
+
+        ]
+    ]));
+    return view('profile', compact('feedItems'));
 });
