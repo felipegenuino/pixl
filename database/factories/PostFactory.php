a3c5552416bd52fdc919de0b27a9fb222e821bbf
Felipe Genuino <design@felipegenuino.com>
2025-12-20 17:16:34 -0300

    modeling reposts

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -22,8 +22,26 @@ class PostFactory extends Factory
             //
             'profile_id' => Profile::factory(),
             'parent_id' => null,
+            'repost_of_id' => null,
             'content' => $this->faker->realText(200),
         ];
+    }
+
+    public function repost(Post $originalPost)
+    {
+        return $this->state([
+            'parent_id' => $originalPost->id,
+            'content' => null,
+        ]);
+    }
+
+
+    public function quotePost(Post $originalPost)
+    {
+        return $this->state([
+            'repost_of_id' => $originalPost->id,
+            'content' => $this->faker->realText(200),
+        ]);
     }
 
     public function reply(Post $parentPost)
